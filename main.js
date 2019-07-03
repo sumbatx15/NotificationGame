1cad5ef04dc79cdd1ac52c1fefd552a1035d4727
sumbat15 <sumbat15@gmail.com>
2019-07-03 14:11:28 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,123 @@
+// target elements with the "draggable" class
+const rand = (num1, num2) => {
+    return Math.floor(Math.random() * num2) + num1
+}
+
+function dragMoveListener(event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+        target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+}
+window.dragMoveListener = dragMoveListener;
+interact('.draggable')
+    .draggable({
+        // enable inertial throwing
+        inertia: true,
+        // keep the element within the area of it's parent
+        // modifiers: [
+        //     interact.modifiers.restrict({
+        //         restriction: "parent",
+        //         endOnly: true,
+        //         elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        //     }),
+        // ],
+        // enable autoScroll
+        autoScroll: true,
+
+        // call this function on every dragmove event
+        onmove: dragMoveListener,
+        // call this function on every dragend event
+        onend: function (event) {
+
+        }
+    });
+
+document.addEventListener('mouseup', ({ target }) => {
+    const item = notifications.find(item => item.el == target)
+    if (item) {
+        item.el.dataset.x < item.startingPosition.x ? console.log('left') : console.log('right')
+        item.el.style.opacity = 0;
+        item.el.style.pointerEvents = 'none';
+    }
+})
+
+const BARS_START_TRANSITION = 'width 500ms cubic-bezier(.61,.17,.52,1.22)'
+const BARS_NORMAL_TRANSITION = 'width 500ms'
+const words = ['growth', 'esteem', 'belong', 'safety', 'energy']
+const notifications = [];
+
+function getTextBars() {
+    return words.reduce((bars, word) => {
+        bars[word] = {
+            el: document.querySelector(`#${word}-bar`),
+            _percent: 100,
+            isStarting: true,
+            get percent() {
+                return this._percent;
+            },
+            set percent(percent) {
+                this._percent = percent
+                this.el.style.transition = this.isStarting ? BARS_START_TRANSITION : BARS_NORMAL_TRANSITION;
+                this.el.style.width = `${percent}%`
+            }
+        }
+        return bars;
+    }, {})
+}
+
+function animateStartGameBars() {
+    words.forEach(word => {
+        const elBar = bars[word].el
+        elBar.style.transition =
+            elBar.style.transitionDelay = '500ms'
+        elBar.style.width = `${startPercent}%`
+    })
+}
+
+window.onload = () => {
+    const eles = Array.from(document.querySelectorAll('.progress'))
+
+    var startPercent = 10
+    eles.forEach(p => {
+        p.style.transition = 'width 500ms cubic-bezier(.61,.17,.52,1.22)'
+        p.style.transitionDelay = '500ms'
+        p.style.width = `${startPercent}%`
+    })
+}
+const imgSrcs = ['growthGood6.png', 'esteemBad1.png']
+setInterval(() => {
+    addDraggable();
+}, 1000);
+function addDraggable() {
+    const prefix = rand(10) > 5 ? 'Good' : 'Bad'
+    const randPicNum = rand(1, words.length);
+    const word = words[rand(0, words.length)]
+    const picName = `assets/notifications/${word}/${word}${prefix}${randPicNum}.svg`
+    console.log('picName:', picName)
+    const [x, y] = [rand(100,1500), rand(100,700)]
+
+    const el = document.createElement('div')
+    el.className = 'draggable'
+    el.style.transform = `translate(${x}px, ${y}px)`
+    el.dataset.x = x;
+    el.dataset.y = y;
+    el.innerHTML = `<img src="${picName}" />`
+
+    const item = {
+        startingPosition: { x, y },
+        el: el
+    }
+
+    notifications.push(item)
+    document.querySelector('.container').appendChild(el)
+}
