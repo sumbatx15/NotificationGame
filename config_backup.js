ede47e10e8f3e574c87922f2931a410c744dd486
sumbat15 <sumbat15@gmail.com>
2019-07-08 15:43:07 +0300

    config

--- config_backup.js
+++ config_backup.js
@@ -1,15 +1,15 @@
 
 // 1second is 1000ms, we work here with ms :)
 // Max volume is 1, mute is 0
-const GOOD_NOTIFICATION_PERCENT = 100 
+const GOOD_NOTIFICATION_PERCENT = 65 
 const BARS_START_PERCENT = 12
 const FEEDBACK_PER_PERCENT = 25
 
-const LOSE_WIN_NOTIFICATION_DELAY = 2000
+const LOSE_WIN_NOTIFICATION_DELAY = 3500
 
-const NOTIFICATION_VOLUME = 0.5
-const FEEDBACK_VOLUME = .1
-const BAR_VOLUME = .1
+const NOTIFICATION_VOLUME = 0.6
+const FEEDBACK_VOLUME = 0.9
+const BAR_VOLUME = 0.6
 
 const TUTORIAL_LENGHT_MS = 20000
 
@@ -21,5 +21,5 @@ const BAR_LOSING_PT = 0.025
 const BAR_LOSING_SPEED_MS = 50
 
 const START_NOTE_SPANW_SPEED_MS = 1000 //Spawning notification every 1000ms wich is 1s
-const END_NOTE_SPANW_SPEED_MS = 600 //Spawning notification every 600ms wich is 0.6s
-const NOTE_SPAWN_ACCELERATOR = 15 
+const END_NOTE_SPANW_SPEED_MS = 500 //Spawning notification every 600ms wich is 0.6s
+const NOTE_SPAWN_ACCELERATOR = 25 
